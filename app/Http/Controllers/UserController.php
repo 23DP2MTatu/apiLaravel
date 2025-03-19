518e068982e2c8d609d648196f0ff77d106378b4
Mark Tatu <a231453mt@rvt.lv>
2025-03-19 11:48:34 +0200

    add resource and request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,70 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
-use App\Models\Users;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()  {
-        $users = Users::all();
-        return response()->json($users);
+        $users = User::all();
+        return UserResource::collection($users);
     }
 
     public function store(request $request) {
-        $users = new Users;
-        $users->username = $request->username;
-        $users->email = $request->email;
-        $users->password = Hash::make($request->password);
-        $users->save();
+
+        $validated = $request->validated();
+    
+        User::create($validated);
+        
         return response()->json([
-            'message' => 'User added.'
+            'message' => 'User added.',
+            'user' => new UserResource($validated),
         ], 201);
     }
 
-    public function show($id) {
-        $user = Users::find($id);
-        if (!empty($user)) {
-            return response()->json($user);
-        } else {
-            return response()->json([
-                "message" => "User not found"
-            ], 404);
-        }
+    public function show(User $user) {
+        return new UserResource($user);
     }
 
-    public function update(Request $request, $id) {
-        if (Users::where('id', $id)->exists()) {
-            $user = Users::find($id);
-            $user -> name = is_null($request->name) ? $user->name : $request->name;
-            $user -> email = is_null($request->email) ? $user->email : $request->email;
-            $user -> password = is_null($request->password) ? $user->password : Hash::make($request->password);
-            
-            $user->save();
-            
-            return response()->json([
-                "message" => "user Updated."
-            ], 404);
-        } else {
-            return response()->json([
-                "message" => "user not found."
-            ], 404);
-        }
+    public function update(Request $request, User $user) {
+    
+        $validated = $request->validated();
+
+        $user->update($validated);
+        
+        return response()->json([
+            'message' => 'User updated',
+            'user' => new UserResource($validated),
+        ]);
     }
 
-    public function destroy($id) {
-        if (Users::where('id',$id)->exists()) {
-            $user = Users::find($id);
-            $user->delete();
-            
-            return response()->json([
-                "message" => "records deleted."
-            ], 202);
-        } else {
-            return response()->json([
-                "message" => "user not found."
-            ], 404);
-        }
+    public function destroy(User $user) {
+        $user->delete();
+        return response()->json([
+            "message" => "records deleted."
+        ], 202);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,26 +2,7 @@
 
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\UserController;
-use App\Models\Users;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/books', function (Request $request) {
-    return $request->books();
-})->middleware('auth:sanctum');
-
-Route::get('/users', function (Request $request) {
-    return $request->users();
-})->middleware('auth:sanctum');
-
-Route::get('/books',[BookController::class, 'index']);
-Route::get('/books/{id}',[BookController::class, 'show']);
-Route::post('/books',[BookController::class, 'store']);
-Route::put('/books/{id}',[BookController::class, 'update']);
-Route::delete('/books/{id}',[BookController::class, 'destroy']);
-
-Route::get('/users',[UserController::class, 'index']);
-Route::get('/users/{id}',[UserController::class, 'show']);
-Route::post('/users',[UserController::class, 'store']);
-Route::put('/users/{id}',[UserController::class, 'update']);
-Route::delete('/users/{id}',[UserController::class, 'destroy']);
+Route::apiResource('books', BookController::class);
+Route::apiResource('user', UserController::class);
